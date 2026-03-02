eb0865219a8c3b7cf4723f5665949f209c527431
farah <farah511181@gmail.com>
2026-03-02 12:33:15 +0530

    fixed frontend path issue

--- Backend/index.js
+++ Backend/index.js
@@ -37,9 +37,9 @@ app.use("/api/v1/job", JobRoute);
 app.use("/api/v1/application", applicationRoute);
 app.use("/api/v1/auth", authRoutes);
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.use(express.static(path.join(_dirname, "../frontend/dist")));
 app.get('*',(_, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(_dirname, "../frontend/dist/index.html"));
 });
 
 
